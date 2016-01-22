150b041f8e3c2f4c702d255ca45adf655ad8cf01
Sam Galison <samface@gmail.com>
2016-01-22 13:17:40 -0500

    failed nlp, successful 2-letter scrambling

--- actions.py
+++ actions.py
@@ -1,4 +1,11 @@
-import random, re
+import random, re, pattern.en as pat
+from django.utils.encoding import smart_str, smart_unicode
+
+
+def cleanstring(sample):
+    # first = re.sub(u"(\u2018|\u2019)", "'", sample)
+    # second = re.sub(u"(\u2026)", "...", first)
+    return final
 
 def rearrange_lines(sample):
     plasma = list(sample)
@@ -6,10 +13,31 @@ def rearrange_lines(sample):
     transfusion = "".join(plasma)
     return transfusion
 
-def garble(sample, cluster_size):
-    blood = sample.read()
-    cleaned = re.sub("[^a-zA-Z]", "", blood)
-    plasma = list(cleaned)
-    random.shuffle(plasma);
-    transfusion = " ".join(plasma)
+def garble(sample, sz):
+    plasma = []
+    transfusion = ""
+
+    if sz == 1:
+        cleaned = re.sub("[^a-zA-Z]", "", sample)
+        plasma = list(cleaned)
+        random.shuffle(plasma)
+        transfusion = " ".join(plasma)
+    elif sz == 2:
+        sounds = r"[aeiouAEIOU][b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z]|[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z][aeiouAEIOU]"
+        for match in re.finditer(sounds, sample):
+            plasma.append(match.group(0))
+            # print match.group(0)
+        random.shuffle(plasma)
+        # print plasma
+        transfusion = " ".join(plasma)
+
+    return transfusion
+
+def peetree(sample):
+    # happiness = cleanstring(sample)
+    tree = pat.parsetree(sample, encoding='utf-8')
+    transfusion = []
+    for sentence in tree:
+        for verb in sentence.verbs:
+            transfusion.append(verb.string)
     return transfusion
--- pythontester.py
+++ pythontester.py
@@ -1,10 +1,16 @@
-import os
+#-*- coding: utf-8 -*-
+
+import os, io
 import actions as ac
 
-print "\ncurrent directory:", os.listdir('.'), "\n"
+print "---------------------------------------------"
+# print "\ncurrent directory:", os.listdir('.'), "\n"
 
 draftcount = 0
 draft_name = ''
+
+sourcefile = "assets/thesis-source-material.txt"
+# sourcefile = "assets/source-test.txt"
 
 
 for draft in os.listdir("output/automated-drafts"):
@@ -15,16 +21,26 @@ for draft in os.listdir("output/automated-drafts"):
 draft_name = "output/automated-drafts/autodraft" + str(draftcount) + ".txt"
 
 def ghostwrite(inspiration):
-    with open(draft_name, "w") as latest_draft:
+    if isinstance(inspiration, list):
+        print "converting from list..."
+        inspiration = " ".join(inspiration).encode("utf-8")
+    with io.open(draft_name, "w", encoding="utf-8") as latest_draft:
         latest_draft.write(inspiration)
         print "wrote " + draft_name
 
 
-vein = open("assets/thesis-source-material.txt")
+with io.open(sourcefile, encoding="utf-8") as source:
+    vein = source.read()
 
 # transfused = ac.rearrange_lines(vein)
-transfused = ac.garble(vein, 1)
+# transfused = ac.garble(vein, 1)
+transfused = ac.garble(vein, 2)
+# transfused = ac.peetree(vein)
+
+# print transfused
 
 ghostwrite(transfused)
 
-print transfused
+
+
+print "---------------------------------------------"
