607abff7cf96df6269b2786c064085f2bfedc82c
Sam Galison <samface@gmail.com>
2016-01-29 10:33:59 -0500

    full word scrambling, interleaving, almost nlp

--- actions.py
+++ actions.py
@@ -1,10 +1,18 @@
 import random, re, pattern.en as pat
 from django.utils.encoding import smart_str, smart_unicode
 
+let = r"[a-zA-Z]" #letters
+con = r"[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z]" #consonants
+vow = r"[aeiouAEIOU]" #vowels
+pun = r"[.,?!-]" #punctuation
+two = r"\s+([a-zA-Z][a-zA-Z])\s+" #two-letter words
+twoend = con + vow + pun + "|" + vow + con + pun #ending noises
+
 
 def cleanstring(sample):
     # first = re.sub(u"(\u2018|\u2019)", "'", sample)
     # second = re.sub(u"(\u2026)", "...", first)
+    final = re.sub(r"\[\s*.+\s*\]", "", sample)
     return final
 
 def rearrange_lines(sample):
@@ -30,14 +38,80 @@ def garble(sample, sz):
         random.shuffle(plasma)
         # print plasma
         transfusion = " ".join(plasma)
+    elif sz == 3:
+        sounds = vow + con + vow + "|" + con + vow + con + "|" + two + "|" + twoend
+        sounds += "|" + vow + vow + con + "|" + con + vow + vow
+        for match in re.finditer(sounds, sample):
+            # print match.group(0)
+            nugget = match.group(1) if match.group(1) is not None else match.group(0)
+            plasma.append(nugget)
+            # print nugget
+        random.shuffle(plasma)
+        # print plasma
+        transfusion = " ".join(plasma)
+    elif sz == 4:
+        sounds = vow + con + vow + pun + "|" + con + vow + con + pun
+        sounds += "|" + vow + vow + con + pun + "|" + con + vow + vow + pun
+
+        for match in re.finditer(sounds, sample):
+            plasma.append(match.group())
+            # print match.group(0)
+
+        sounds = let + let + let + let
+        for match in re.finditer(sounds, sample):
+            plasma.append(match.group())
+            # print match.group(0)
+
+        random.shuffle(plasma)
+        # print plasma
+        transfusion = " ".join(plasma)
+    elif sz >= 5:
+        sounds = let +"{"+ str(sz-1) +"}"+ pun
+        sounds += "|" + let + "{"+ str(sz) +"}"
+
+        for match in re.finditer(sounds, sample):
+            plasma.append(match.group())
+            # print match.group(0)
+
+        random.shuffle(plasma)
+        # print plasma
+        transfusion = " ".join(plasma)
+
 
     return transfusion
 
 def peetree(sample):
-    # happiness = cleanstring(sample)
-    tree = pat.parsetree(sample, encoding='utf-8')
-    transfusion = []
+    plasma = []
+    transfusion = ""
+
+    happiness = cleanstring(sample)
+    tree = pat.parsetree(happiness, replace={})
     for sentence in tree:
-        for verb in sentence.verbs:
-            transfusion.append(verb.string)
+        plasma.append(unicode(sentence.string))
+        # for verb in sentence.verbs:
+            # transfusion.append(unicode(verb.string))
+            # print verb.string
+    random.shuffle(plasma)
+    transfusion = "\n".join(plasma)
     return transfusion
+
+def leaver(one, two, howfarthrough, spacing=1, ramp=True):
+    print one, "\n", two
+
+    start = int(howfarthrough * len(one))
+
+    print "index of start: ", start
+
+    keeptracker = start
+
+    for nugget in two:
+        if keeptracker < len(one):
+            one.insert(keeptracker, nugget)
+            keeptracker += spacing
+            if ramp == True:
+                if spacing > 1: spacing -= 1
+        else:
+            one.append(nugget)
+        keeptracker += 1
+
+    print one
--- pythontester.py
+++ pythontester.py
@@ -7,40 +7,74 @@ print "---------------------------------------------"
 # print "\ncurrent directory:", os.listdir('.'), "\n"
 
 draftcount = 0
+pigcount = 0
 draft_name = ''
+pig_name = ''
 
-sourcefile = "assets/thesis-source-material.txt"
-# sourcefile = "assets/source-test.txt"
+# sourcefile = "assets/thesis-source-material.txt"
+sourcefile = "assets/source-test.txt"
 
 
 for draft in os.listdir("output/automated-drafts"):
     if draft.startswith("autodraft"):
         # print draft
         draftcount += 1
+    if draft.startswith("pigpile"):
+        # print draft
+        pigcount += 1
 
 draft_name = "output/automated-drafts/autodraft" + str(draftcount) + ".txt"
+pig_name = "output/automated-drafts/pigpile" + str(pigcount) + ".txt"
 
 def ghostwrite(inspiration):
     if isinstance(inspiration, list):
         print "converting from list..."
-        inspiration = " ".join(inspiration).encode("utf-8")
+        inspiration = unicode(" ".join(inspiration), "utf-8")
+    elif isinstance(inspiration, str):
+        print "incoming string, making unicode..."
+        inspiration = unicode(inspiration, "utf-8")
     with io.open(draft_name, "w", encoding="utf-8") as latest_draft:
         latest_draft.write(inspiration)
         print "wrote " + draft_name
+
+def pigpile(inspiration):
+    if isinstance(inspiration, list):
+        print "converting from list..."
+        inspiration = " " + " ".join(inspiration).encode("utf-8")
+    elif isinstance(inspiration, str):
+        inspiration = unicode(inspiration, "utf-8")
+    with io.open(pig_name, "a", encoding="utf-8") as latest_draft:
+        latest_draft.write(inspiration)
+        print "wrote " + pig_name
 
 
 with io.open(sourcefile, encoding="utf-8") as source:
     vein = source.read()
 
 # transfused = ac.rearrange_lines(vein)
-# transfused = ac.garble(vein, 1)
-transfused = ac.garble(vein, 2)
-# transfused = ac.peetree(vein)
+
+transfused = ac.peetree(vein)
+# for i in range(20):
+    # transfused = ac.garble(vein, i+1)
+    # transfused += " "
+    # pigpile(transfused)
+    # print transfused
+
+
+one = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+two = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+ac.leaver(one, two, 0.5, spacing=4)
+
+# ghostwrite(transfused)
+# os.system("open " + draft_name)
+
+# transfused = ac.garble(vein, 6)
+
+
+
+# pigpile(transfused)
 
 # print transfused
-
-ghostwrite(transfused)
-
 
 
 print "---------------------------------------------"
